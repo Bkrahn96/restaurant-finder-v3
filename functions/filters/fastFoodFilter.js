27d9ef2051cf04317f102cf5dbce3cf6afe6b275
krahn <bkrahnhome@gmail.com>
2024-07-20 13:35:06 -0400

    Updated fast food filter to avoid explicit chain names and include smoothies. Improved keyword search.

--- functions/filters/fastFoodFilter.js
+++ functions/filters/fastFoodFilter.js
@@ -1,11 +1,14 @@
 // functions/filters/fastFoodFilter.js
-const fastFoodKeywords = ["burger", "chicken", "sandwich", "fries", "fast food", "wendy's", "dairy queen"];
+const fastFoodKeywords = ["burger", "chicken", "sandwich", "fries", "fast food", "smoothie"];
 
 function fastFoodFilter(restaurant) {
     const excludeTypes = ["bar", "home_goods_store"];
+    const chainNames = ["mcdonald's", "burger king", "wendy's", "dairy queen"];
+
     return (
         fastFoodKeywords.some(keyword => restaurant.name.toLowerCase().includes(keyword)) &&
         !excludeTypes.some(excludeType => restaurant.types.includes(excludeType)) &&
+        !chainNames.some(chain => restaurant.name.toLowerCase().includes(chain)) &&
         (restaurant.types.includes("bakery") ? restaurant.types.includes("cafe") : true)
     );
 }

feaef786d7cea357f9cf161bf051340ef146a4a1
krahn <bkrahnhome@gmail.com>
2024-07-20 13:38:33 -0400

    Improve fast food filter, maintain 'More' button functionality, and update filter scripts

--- functions/filters/fastFoodFilter.js
+++ functions/filters/fastFoodFilter.js
@@ -1,14 +1,13 @@
 // functions/filters/fastFoodFilter.js
-const fastFoodKeywords = ["burger", "chicken", "sandwich", "fries", "fast food", "smoothie"];
+const fastFoodKeywords = ["burger", "chicken", "sandwich", "fries", "smoothie"];
 
 function fastFoodFilter(restaurant) {
     const excludeTypes = ["bar", "home_goods_store"];
-    const chainNames = ["mcdonald's", "burger king", "wendy's", "dairy queen"];
-
     return (
-        fastFoodKeywords.some(keyword => restaurant.name.toLowerCase().includes(keyword)) &&
+        (fastFoodKeywords.some(keyword => restaurant.name.toLowerCase().includes(keyword)) ||
+        restaurant.types.includes("meal_takeaway") ||
+        restaurant.types.includes("fast_food")) &&
         !excludeTypes.some(excludeType => restaurant.types.includes(excludeType)) &&
-        !chainNames.some(chain => restaurant.name.toLowerCase().includes(chain)) &&
         (restaurant.types.includes("bakery") ? restaurant.types.includes("cafe") : true)
     );
 }
